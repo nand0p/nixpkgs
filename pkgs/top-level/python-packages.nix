7d07139873e36f439126e3b419961a319abbff7d
Fernando J Pando <fernando.pando@stelligent.com>
2016-08-18 10:44:20 -0400

    future: future-0.15.2
    Enable source mirror and all platforms.
    
    Tested on Linux
    Tested on Darwin

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10780,34 +10780,25 @@ in modules // {
 
   future = buildPythonPackage rec {
     version = "0.15.2";
-    name = "future-${version}";
+    pname = "future";
+    name = "${pname}-${version}";
 
     src = pkgs.fetchurl {
-      url = "http://github.com/PythonCharmers/python-future/archive/v${version}.tar.gz";
+      url = "mirror://pypi/f/${pname}/${name}.tar.gz";
       sha256 = "0vm61j5br6jiry6pgcxnwvxhki8ksnirp7k9mcbmxmgib3r60xd3";
     };
 
     propagatedBuildInputs = with self; optionals isPy26 [ importlib argparse ];
+
     doCheck = false;
+    # URLError: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed (_ssl.c:590)>
 
     meta = {
       description = "Clean single-source support for Python 3 and 2";
-      longDescription = ''
-        python-future is the missing compatibility layer between Python 2 and
-        Python 3. It allows you to use a single, clean Python 3.x-compatible
-        codebase to support both Python 2 and Python 3 with minimal overhead.
-
-        It provides future and past packages with backports and forward ports
-        of features from Python 3 and 2. It also comes with futurize and
-        pasteurize, customized 2to3-based scripts that helps you to convert
-        either Py2 or Py3 code easily to support both Python 2 and 3 in a
-        single clean Py3-style codebase, module by module.
-      '';
-      homepage = https://python-future.org;
-      downloadPage = https://github.com/PythonCharmers/python-future/releases;
+      homepage = "https://python-future.org";
       license = licenses.mit;
-      maintainers = with maintainers; [ prikhi ];
-      platforms = platforms.linux;
+      maintainers = with maintainers; [ prikhi nand0p ];
+      platforms = platforms.all;
     };
   };
 
