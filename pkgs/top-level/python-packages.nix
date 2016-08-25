341c94466f1e1fb3cb22c5583fbef29bb578911a
Fernando J Pando <fernando.pando@stelligent.com>
2016-08-25 12:04:33 -0400

    jsonref: init jsonref-0.1
    Tested on Linux
    Tested on Darwin

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -29615,4 +29615,23 @@ in modules // {
       platforms   = platforms.all;
     };
   };
+
+jsonref = buildPythonPackage rec {
+    name = "${pname}-${version}";
+    pname = "jsonref";
+    version = "0.1";
+
+    meta = {
+      description = "An implementation of JSON Reference for Python.";
+      homepage    = "http://github.com/gazpachoking/jsonref";
+      license     = licenses.mit;
+      maintainers = with maintainers; [ nand0p ];
+      platforms   = platforms.all;
+    };
+
+    src = pkgs.fetchurl {
+      url = "mirror://pypi/j/${pname}/${name}.tar.gz";
+      sha256 = "1lqa8dy1sr1bxi00ri79lmbxvzxi84ki8p46zynyrgcqhwicxq2n";
+    };
+  };
 }
