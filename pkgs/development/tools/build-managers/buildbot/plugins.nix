ee31292897f6a4e49c836a44309b5c5b1c290c25
Fernando J Pando <fernando.pando@stelligent.com>
2016-08-25 22:04:07 -0400

    buildbot: 0.9.0rc1 -> 0.9.0rc2

--- pkgs/development/tools/build-managers/buildbot/plugins.nix
+++ pkgs/development/tools/build-managers/buildbot/plugins.nix
@@ -6,11 +6,11 @@
 let
   buildbot-pkg = pythonPackages.buildPythonPackage rec {
     name = "buildbot-pkg-${version}";
-    version = "0.9.0rc1";
+    version = "0.9.0rc2";
 
     src = fetchurl {
-      url = "https://pypi.python.org/packages/8f/37/6de8734299b94c6d853d2380f0a2cdadf00d24bbac6b8947ea2d3848d15d/${name}.tar.gz";
-      sha256 = "0bbbf10361087493cc6ee62e077863eafe1b3e24c43a8429a6eb1a75b35359b6";
+      url = "https://pypi.python.org/packages/95/47/1fef931d410cc24127564c2e193e7c1c184f5c5f481930f77c6d6840cfab/${name}.tar.gz";
+      sha256 = "01wc9bmqq1rfayqnjm7rkjhbcj7h6ah4vv10s6hglnq9s4axvxp6";
     };
 
     propagatedBuildInputs = with pythonPackages; [ setuptools ];
@@ -32,13 +32,13 @@ in {
 
   www = pythonPackages.buildPythonPackage rec {
     name = "buildbot_www-${version}";
-    version = "0.9.0rc1";
+    version = "0.9.0rc2";
 
     # NOTE: wheel is used due to buildbot circular dependency
     format = "wheel";
     src = fetchurl {
-      url = "https://pypi.python.org/packages/f4/3c/8bf1add762739d1a15d63c91140d7fdc32d872860229ffdb10e735fdaa7a/${name}-py2-none-any.whl";
-      sha256 = "21c3b55be0c1622757a04e6573e8503f2222ba80f01af3770eb04d71e3c5a8fb";
+      url = "https://pypi.python.org/packages/e0/d7/f1023cdb7340a15ee1fc9916e87c4d634405a87164a051e2c59bf9d51ef1/${name}-py2-none-any.whl";
+      sha256 = "1006x56x4w4p2mbrzm7jy51c0xxz48lzhdwvx7j4hrjs07mapndj";
     };
 
     propagatedBuildInputs = [ buildbot-pkg ];
@@ -53,11 +53,11 @@ in {
 
   console-view = pythonPackages.buildPythonPackage rec {
     name = "buildbot-console-view-${version}";
-    version = "0.9.0rc1";
+    version = "0.9.0rc2";
 
     src = fetchurl {
       url = "https://pypi.python.org/packages/ac/d4/33b0465d9e1c2d2d098bbf2b44067e7cb626fab89fe1af732143e4a2359f/${name}.tar.gz";
-      sha256 = "4cd6c276082a65d2a7d6c9f8fbc14c9f7a57f80ca6ffa09b111976e026ab9d3c";
+      sha256 = "41d6c276082a65d2a7d6c9f8fbc14c9f7a57f80ca6ffa09b111976e026ab9d3c";
     };
 
     propagatedBuildInputs = [ buildbot-pkg ];
@@ -72,11 +72,11 @@ in {
 
   waterfall-view = pythonPackages.buildPythonPackage rec {
     name = "buildbot-waterfall-view-${version}";
-    version = "0.9.0rc1";
+    version = "0.9.0rc2";
 
     src = fetchurl {
       url = "https://pypi.python.org/packages/34/0b/267f65cf865f25b3616e3231111028181c6cc0c74d983ff17b98721a9992/${name}.tar.gz";
-      sha256 = "8822f75ceac242d00dc10cdc381864e460b936532a1618bf47ce9b353a63814a";
+      sha256 = "1122f75ceac242d00dc10cdc381864e460b936532a1618bf47ce9b353a63814a";
     };
 
     propagatedBuildInputs = [ buildbot-pkg ];
