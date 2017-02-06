68e6ac76165413d7048e68bee101591acb1cf7e9
Fernando J Pando <fernando.pando@stelligent.com>
2017-02-06 15:27:24 -0500

    treq: init 16.12.1
    Tested on NixOS

---
+++ pkgs/development/python-modules/treq/default.nix
@@ -0,0 +1,26 @@
+{ stdenv, pkgs }:
+
+pkgs.python27Packages.buildPythonPackage rec {
+   name = "${pname}-${version}";
+   pname = "treq";
+   version = "16.12.0";
+
+   src = pkgs.fetchurl {
+     url = "mirror://pypi/t/${pname}/${name}.tar.gz";
+     sha256 = "1aci3f3rmb5mdf4s6s4k4kghmnyy784cxgi3pz99m5jp274fs25h";
+   };
+
+   propagatedBuildInputs = with pkgs.python27Packages; [
+     service-identity
+     requests2
+     twisted
+     incremental
+   ];
+
+   meta = with stdenv.lib; {
+     homepage = http://github.com/twisted/treq;
+     description = "A requests-like API built on top of twisted.web's Agent";
+     license = licenses.mit;
+     maintainers = maintainers.nando0p;
+   };
+}
